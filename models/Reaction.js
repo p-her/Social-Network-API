05512e8013fb915a7fb755d6b6efc7250283699b
Pao Her <p.her007@hotmail.com>
2022-04-01 17:32:57 -0500

    finished with models

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+
+
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: date => dataFormat(date, 'dddd,mmm dS, yyyy')
+        }
+    }
+)
--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,64 @@
 
 const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: date => dataFormat(date, 'dddd,mmm dS, yyyy')
+        },
+        username: [
+            {
+                type: String,
+                required: true
+            }
+        ],
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
+
+ThoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+})
+
+
+
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: date => dataFormat(date, 'dddd,mmm dS, yyyy')
+        }
+    }
+)
+
+const Thought = model('Thought', ThoughtSchema);
+module.exports = Thought;
